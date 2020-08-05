43b7d83382c02bbc1a9faccefc6b1c91e6a642cd
supremovic <58049412+supremovic@users.noreply.github.com>
2020-08-05 20:04:49 -0300

    Updates

---
+++ precache-manifest.d45819720ad7ab2060c471bf48844c27.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b282b77ad91b67705ddbce8accaf698",
+    "url": "/rday/index.html"
+  },
+  {
+    "revision": "7f40ff155ae2c553aaea",
+    "url": "/rday/static/css/main.5e88452c.chunk.css"
+  },
+  {
+    "revision": "9d58863d1f7e166eaa42",
+    "url": "/rday/static/js/2.114239e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rday/static/js/2.114239e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f40ff155ae2c553aaea",
+    "url": "/rday/static/js/main.f841dd8b.chunk.js"
+  },
+  {
+    "revision": "17bc205b6dbc23a59ca7",
+    "url": "/rday/static/js/runtime-main.78caa078.js"
+  },
+  {
+    "revision": "8f5ae299a38780b68b26b104b7b8baa8",
+    "url": "/rday/static/media/cake.8f5ae299.png"
+  },
+  {
+    "revision": "c62a32ed7bc4db21b4170589e9eca78c",
+    "url": "/rday/static/media/ricardo-dancing.c62a32ed.gif"
+  },
+  {
+    "revision": "88ad6787bd352c445c2386c5b7fe9f5f",
+    "url": "/rday/static/media/switch.88ad6787.png"
+  }
+]);
